6eb07dee260d631f188693ff369af7ccd165a139
Abinshah Ameer <abinshahcoolz@gmail.com>
2020-07-01 00:56:53 +0530

    link creation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -22,7 +22,8 @@ function App() {
             <button onClick={openMenu}>
               &#9776;
             </button>
-            <a href="index.html">amazona</a>
+            <Link to="/">amazona</Link>
+            
           </div>
           <div className="header-links">
             <a href="cart.html">Cart</a>
@@ -47,29 +48,7 @@ function App() {
           <div className="content">
           <Route path="/product/:id" component={ProductScreen} />
           <Route path="/" exact={true} component={HomeScreen} />
-            <ul className="products">
-           
-                  {
-                   data.products.map(product =>
-                   <li>
-                     <div className="product">
-                
-
-                    <img className="product-image" src={product.image} alt="product" />
-                    <div className="product-name">
-                      <a href="product.html">{product.name}</a>
-                    </div>
-                    <div className="product-brand">{product.brand}</div>
-                   <div className="product-price">{product.price}</div>
-                   <div className="product-rating">{product.rating} stars {product.numReviews}</div>
-                  </div>
-                </li>
-                )
-                  }
-              
-              
-    
-            </ul>
+         
           </div>
     
         </main>
