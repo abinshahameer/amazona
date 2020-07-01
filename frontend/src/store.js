8d4062dcdbeda21a9a059ff6f95614fd5733b3c4
Abinshah Ameer <abinshahcoolz@gmail.com>
2020-07-01 23:00:05 +0530

    redux configuration

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,1 +1,8 @@
-import {createStore  } from 'redux' 
+import {createStore, combineReducers, compose  } from 'redux' 
+import { productListReducer } from './reducers/productReducers';
+const initialState={};
+const reducer=combineReducers({
+    productList: productListReducer,
+})
+const store=createStore(reducer,initialState,compose(applyMiddleware(thunnk)));
+export default store;
