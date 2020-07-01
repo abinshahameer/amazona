243c80da84408a5856a746327aff0e73d4c6393a
Abinshah Ameer <abinshahcoolz@gmail.com>
2020-07-01 21:24:54 +0530

    nodemon integration and configuring express

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,7 @@
 import express from 'express';
+import data from './data'
 const app=express();
-app.get("/api/products",(req,res))
+app.get("/api/products",(req,res)=>{
+    res.send(data.products);
+});
+app.listen(5000,()=>{console.log("server started at http://localhost:5000/api/products")})
