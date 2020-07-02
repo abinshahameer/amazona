279ccb5d0129d1cd8a7121452c0e9081da0448f8
Abinshah Ameer <abinshahcoolz@gmail.com>
2020-07-03 00:07:55 +0530

    changing fetch data to const dispatch

--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -2,9 +2,13 @@ import React, { useState, useEffect } from 'react';
 //import data from '../data'
 import { Link } from 'react-router-dom';
 import axios from 'axios' 
+import { useSelector } from 'react-redux'
 function HomeScreen(props){
 
   const[products, setProduct]= useState([]);
+  const productList=useSelector(state=>state.productList);
+  const {products,loading,error}=productList;
+  const dispatch = useDispatch(function);
 
   useEffect(() => {
     const fetchData=async ()=>{
